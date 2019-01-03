442354627baa5203625d32645dc9712a4505fa05
haugstve <daniel.haugstvedt@gmail.com>
2019-01-03 15:48:05 +0100

    Add files created by cookiecutter
    The CookieCutter template for Data Science makes a directory. It contains a setup that makes
    you projects easier to come back to in the future. With a known folder structuer and a known way to add files to the project is is easier for others to understand what you are doing and,
    not least, for you to remember what you did.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To get the badge I need to complete a capstone project',
+    author='Daniel Haugstvedt',
+    license='BSD-3',
+)
